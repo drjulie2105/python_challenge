753d1dde00fa377fea95234396ef9a7929c2f949
Julie Pyle <drjuliepyle@gmail.com>
2020-08-11 09:37:07 -0400

    Calculated total votes for each candidate

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,8 +5,11 @@ electiondata_csv = os.path.join('..', 'PyPoll', 'Resources', 'election_data.csv'
 
 voter_id=[]
 county = []
-candidates=["Khan", "Correy", "Li", "O'Tooley"]
-
+candidates = []
+khan=[]
+correy=[]
+li=[]
+otooley=[]
 
 
 with open(electiondata_csv) as csv_file:
@@ -16,11 +19,18 @@ with open(electiondata_csv) as csv_file:
     for row in csv_reader:
         voter_id.append(row[0])
         total_votes = len(voter_id)
-
-
-
-
-
+        if row[2] == "Khan":
+            khan.append(row[2])
+        elif row[2] == "Correy":
+            correy.append(row[2])
+        elif row[2] == "Li":
+            li.append(row[2])
+        else:
+            otooley.append(row[2])
+        total_khan = len(khan)
+        total_correy = len(correy)
+        total_li = len(li)
+        total_otooley = len(otooley)
 
 
 
@@ -28,10 +38,10 @@ print ("Election Results")
 print("---------------------------------------")
 print ("Total Votes: " + str(total_votes))
 print("---------------------------------------")
-print("Khan: ")
-print ("Correy: ")
-print("Li: ")
-print("O'Tooley: ")
+print("Khan: " + str(total_khan))
+print ("Correy: " + str(total_correy))
+print("Li: " + str(total_li))
+print("O'Tooley: " + str(total_otooley))
 print("---------------------------------------")
 print("Winner: ")
 print("---------------------------------------")
