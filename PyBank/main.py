a79370d7f816e2a5f80f03e1777ea6b411ab3934
Julie Pyle <drjuliepyle@gmail.com>
2020-08-13 07:05:52 -0400

    Recalculated Average Change, Greatest Increase and Greatest Decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,8 +5,14 @@ budgetdata_csv = os.path.join('..', 'PyBank', 'Resources', 'budget_data.csv')
 
 profit_loss=[]
 date = []
-greatest_increase_month=[]
-greatest_decrease_month=[]
+changes = []
+months = []
+
+number_month = 0
+net_profitloss = 0
+last_month_profitloss = 0
+this_month_profitloss = 0
+change = 0
 
 with open(budgetdata_csv) as csv_file:
     csv_reader= csv.reader(csv_file, delimiter=",")
@@ -15,22 +21,37 @@ with open(budgetdata_csv) as csv_file:
     for row in csv_reader:
         profit_loss.append(int(row[1]))
         date.append(row[0])
-        months = len(date)
+        total_months = len(date)
         total_profitloss=sum(profit_loss, 0)
-        average_change = round(total_profitloss/months)
-
-    
-greatest_increase = max(profit_loss)
-greatest_decrease = min(profit_loss)
-
-greatest_increase_month=max(profit_loss), row[0]
-greatest_decrease_month=min(profit_loss), row[0]
 
 
+        number_month += 1
+        this_month_profitloss = int(row[1])
+        net_profitloss += this_month_profitloss
 
-print ("Financial Analysis")
+        if (number_month==1):
+            last_month_profitloss = this_month_profitloss
+            continue
+        
+        else:
+            change = this_month_profitloss - last_month_profitloss
+            months.append(row[0])
+            changes.append(change)
+            last_month_profitloss = this_month_profitloss
+        
+        sum_changes = sum(changes)
+        average_change = round(sum_changes/(number_month-1), 2)
+        greatest_increase = max(changes)
+        greatest_decrease = min(changes)
+        greatest_increase_index=changes.index(greatest_increase)
+        greatest_decrease_index=changes.index(greatest_decrease)
+        greatest_increase_month=months[greatest_increase_index]
+        greatest_decrease_month=months[greatest_decrease_index]
+        
+
+print("Financial Analysis")
 print("---------------------------------------")
-print("Total Months: " + str(months))
+print("Total Months: " + str(total_months))
 print("Total: $" + str(total_profitloss))
 print("Average Change: $" + str(average_change))
 print(f"Greatest Increase in Profits: " + str(greatest_increase_month) + " ($" + str(greatest_increase) + ")")
@@ -44,7 +65,7 @@ with open(output_file, "w") as file:
     file.write("\n")
     file.write("---------------------------------------")
     file.write("\n")
-    file.write("Total Months: " + str(months))
+    file.write("Total Months: " + str(total_months))
     file.write("\n")
     file.write("Total: $" + str(total_profitloss))
     file.write("\n")
